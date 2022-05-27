babe3837dbd6fe13c8009cdfd3062c80b36ef956
Linas Mintautiškis <linas.mintautiskis@gmail.com>
2022-05-27 19:35:25 +0300

    greicio testas

--- Vector.cpp
+++ Vector.cpp
@@ -5,20 +5,39 @@
 #include <numeric>
 #include <string>
 #include <vector>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
+
+void pushtestas1(int sz)
+{
+    auto start = chrono::high_resolution_clock::now();
+    std::vector<int> v1;
+    for (int i = 1; i <= sz; ++i)
+        v1.push_back(i);
+    auto duration = duration_cast<microseconds>(chrono::high_resolution_clock::now() - start);
+    cout << sz << " std::vector uztruko: " << float(duration.count()) / 1000000 << endl;
+}
+void pushtestas2(int sz)
+{
+    auto start = chrono::high_resolution_clock::now();
+    Vector<int> v2;
+    for (int i = 1; i <= sz; ++i)
+        v2.push_back(i);
+    auto duration = duration_cast<microseconds>(chrono::high_resolution_clock::now() - start);
+    cout <<sz<< " Vector uztruko: " << float(duration.count()) / 1000000<<endl;
+}
 
 int main()
 {
-        std::vector<int> numbers;
-        std::cout << "Initially, numbers.empty(): " << numbers.empty() << '\n';
-
-        numbers.push_back(42);
-        std::cout << "After adding elements, numbers.empty(): " << numbers.empty() << '\n';
-
-        Vector<int> numbers1;
-        std::cout << "Initially, numbers1.empty(): " << numbers1.empty() << '\n';
-
-        numbers1.push_back(42);
-        std::cout << "After adding elements, numbers1.empty(): " << numbers1.empty() << '\n';
+    for (int i = 10000; i <= 100000000; i *= 10)
+    {
+        pushtestas1(i);
+    }
+    cout << endl;
+    for (int i = 10000; i <= 100000000; i *= 10)
+    {
+        pushtestas2(i);
+    }
 }
