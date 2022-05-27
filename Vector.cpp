211186df32c137bea85f592bdcbb7ad24266265a
Linas Mintautiškis <linas.mintautiskis@gmail.com>
2022-05-27 18:11:29 +0300

    Funkciju testavimas

--- Vector.cpp
+++ Vector.cpp
@@ -4,63 +4,39 @@
 #include <iostream>
 #include <numeric>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-
 int main()
 {
-    Vector<int> v;
-    for (int i = 0; i < 11; ++i)
-    {
-        v.push_back(i);
-    }
-    Vector<int> w;
-    w.push_back(4);
-    cout << w<<endl;
-    w = v;
-    cout << w << endl;
-    w.erase(4);
-    w.erase(7);
-    cout << w<<endl;
+    std::vector<int> foo(3, 100);
+    std::vector<int> bar(5, 200);
 
-    w.insert(4, 9);
-    w.insert(4, 9);
-    w.insert(4, 9);
-    w.insert(4, 9);
-    w.insert(4, 9);
-    w.insert(4, 9);
-    w.insert(4, 9);
-    cout << w << endl;
-    w.resize(4);
-    cout << w << endl;
-    w.resize(7);
-    cout << w << endl;
-    w.resize(9,5);
-    cout << w << endl;
-    Vector<int>a{ 1, 2, 4, 8, 16 };
-    w.swap(a);
-    cout << w<<endl;
-    cout << a << endl;
+    foo.swap(bar);
 
-        //Vector<int> nums{ 1, 2, 4, 8, 16 };
-        //Vector<std::string> fruits{ "orange", "apple", "raspberry" };
-        //Vector<char> empty;
+    std::cout << "foo contains:";
+    for (unsigned i = 0; i < foo.size(); i++)
+        std::cout << ' ' << foo[i];
+    std::cout << '\n';
 
-        //// Print vector.
-        //std::for_each(nums.begin(), nums.end(), [](const int n) { std::cout << n << ' '; });
-        //std::cout << '\n';
+    std::cout << "bar contains:";
+    for (unsigned i = 0; i < bar.size(); i++)
+        std::cout << ' ' << bar[i];
+    std::cout << '\n';
 
-        //// Sums all integers in the vector nums (if any), printing only the result.
-        //std::cout << "Sum of nums: "
-        //    << std::accumulate(nums.begin(), nums.end(), 0) << '\n';
+    Vector<int> foo1(3, 100);
+    Vector<int> bar1(5, 200);
 
-        //// Prints the first fruit in the vector fruits, checking if there is any.
-        //if (!fruits.empty())
-        //    //std::cout << "First fruit: " << *fruits.begin() << '\n';
+    foo1.swap(bar1);
 
-        //if (empty.begin() == empty.end())
-        //    std::cout << "vector 'empty' is indeed empty.\n";
-  
-    return 0;
+    std::cout << "foo1 contains:";
+    for (unsigned i = 0; i < foo1.size(); i++)
+        std::cout << ' ' << foo1[i];
+    std::cout << '\n';
+
+    std::cout << "bar1 contains:";
+    for (unsigned i = 0; i < bar1.size(); i++)
+        std::cout << ' ' << bar1[i];
+    std::cout << '\n';
 }
